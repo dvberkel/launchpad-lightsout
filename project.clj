d047f9ab7c6fd57b9d303df712816603f74ce09b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-08 20:03:30 +0200

    Depend on clj-launchpad

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/dvberkel/launchpad-lightsout"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-launchpad "0.3.0"]]
   :main ^:skip-aot launchpad-lightsout.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
