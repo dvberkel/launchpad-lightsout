fd35bc4632ea12f7ea1056769487c34272d00f5b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-09 20:38:21 +0200

    Create cheatsheet for REPL

---
+++ src/launchpad_lightsout/game.clj
@@ -0,0 +1,18 @@
+(ns launchpad-lightsout.game
+  (use [clj-launchpad :only [open on-grid-pressed reset close]])
+  (use [launchpad-lightsout.grid :only [lights-out-grid]])
+  (use [launchpad-lightsout.press :only [press]])
+  (use [launchpad-lightsout.launchpad :only [push]]))
+
+(def grid (atom (lights-out-grid 5)))
+
+(def lpad (open "Launchpad Mini"))
+
+(defn lights-out-handler [x y pressed?]
+  (do
+    (swap! grid (fn [grid] (press grid x y 3)))
+    (push lpad @grid)))
+
+(on-grid-pressed lpad lights-out-handler)
+
+(close lpad)
