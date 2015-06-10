d871118b5aa81291e384b150d87be0e1aa8c3131
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-10 07:41:45 +0200

    Create a setup grid

--- src/launchpad_lightsout/grid.clj
+++ src/launchpad_lightsout/grid.clj
@@ -1,10 +1,22 @@
 (ns launchpad-lightsout.grid)
 
+(defn- create-grid [m n value]
+  "Creates a m x n grid filled with value"
+  (let [row (vec (map (constantly value) (range n)))]
+     (vec (map (constantly row) (range m)))))
+
 (defn lights-out-grid
   ([n]
    "Creates a symmetric n x n lights out grid"
    (lights-out-grid n n))
   ([m n]
    "Creates a m x n lights out grid"
-   (let [row (vec (map (constantly 0) (range n)))]
-     (vec (map (constantly row) (range m))))))
+   (create-grid m n 0)))
+
+(defn setup-grid
+  ([m n]
+   "Creates a m x n setup grid with 2 colors"
+   (setup-grid m n 2))
+  ([m n colors]
+   "Creates a m x n setup grid with colors"
+   (create-grid m n (dec colors))))
--- test/launchpad_lightsout/grid_test.clj
+++ test/launchpad_lightsout/grid_test.clj
@@ -14,3 +14,10 @@
     (is (= [[0 0] [0 0] [0 0]] (lights-out-grid 3 2)))
     (is (= [[0 0 0]] (lights-out-grid 1 3)))
     (is (= [[0 0 0] [0 0 0]] (lights-out-grid 2 3)))))
+
+(deftest setup-test
+  (testing "if a rectangular setup grid can be made"
+    (is (= [[1]] (setup-grid 1 1)))
+    (is (= [[1] [1]] (setup-grid 2 1)))
+    (is (= [[1 1]] (setup-grid 1 2)))
+    (is (= [[1 1] [1 1]] (setup-grid 2 2)))))
