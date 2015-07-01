cfbdf2f7b82c671e86d915342495a13a80cdc437
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-01 19:54:05 +0100

    Restrict handlers to operate on main grid

--- src/launchpad_lightsout/game.clj
+++ src/launchpad_lightsout/game.clj
@@ -24,30 +24,33 @@
   "Create a setup handler to determine game"
   (let [next-color {2 3 3 4 4 2}]
     (fn [x y pressed?]
-      (let [row (inc x) column (inc y)]
-        (do
-          (if pressed?
-            (let [colors (if (and (= row (@game :rows)) (= column (@game :columns)))
-                           (get next-color (@game :colors) 2)
-                           2)]
-              (swap! game (fn [game] (create-game row column colors)))))
-          (push lpad (@game :grid)))))))
+      (if (and (< x 8) (< y 8))
+        (let [row (inc x) column (inc y)]
+          (do
+            (if pressed?
+              (let [colors (if (and (= row (@game :rows)) (= column (@game :columns)))
+                             (get next-color (@game :colors) 2)
+                             2)]
+                (swap! game (fn [game] (create-game row column colors)))))
+            (push lpad (@game :grid))))))))
 
 (defn create-problem-handler [lpad game]
   "Create a problem handler that allows to setup a Lights Out problem"
   (fn [x y pressed?]
-    (do
-      (if pressed? (swap! game (fn [game]
-                                 (assoc game :grid (safe-increment (game :grid) x y (game :colors))))))
-      (push lpad (@game :grid)))))
+    (if (and (< x 8) (< y 8))
+      (do
+        (if pressed? (swap! game (fn [game]
+                                   (assoc game :grid (safe-increment (game :grid) x y (game :colors))))))
+        (push lpad (@game :grid))))))
 
 (defn create-lights-out-handler [lpad game]
   "create a lights out handler that cycles through a number of colors"
   (fn [x y pressed?]
-    (do
-      (if pressed? (swap! game (fn [game]
-                                 (assoc game :grid (press (game :grid) x y (game :colors))))))
-      (push lpad (@game :grid)))))
+    (if (and (< x 8) (< y 8))
+      (do
+        (if pressed? (swap! game (fn [game]
+                                   (assoc game :grid (press (game :grid) x y (game :colors))))))
+        (push lpad (@game :grid))))))
 
 (defn reset-game [game]
   "Resets the grid to be empty"
